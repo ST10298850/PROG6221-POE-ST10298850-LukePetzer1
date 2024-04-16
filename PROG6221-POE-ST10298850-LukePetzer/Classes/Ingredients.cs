791dffe3712142e648756138f67bf3ea4b6f8e2f
Luke Petzer <129840160+ST10298850@users.noreply.github.com>
2024-04-16 19:03:59 +0200

    PROG POE

---
+++ PROG6221-POE-ST10298850-LukePetzer/Classes/Ingredients.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10298850_POE_LukePetzer.Classes
+{
+    // Class for representing an ingredient
+    internal class Ingredient
+    {
+        // Private fields
+        private string name;
+        private double quantity;
+        private string unitOfMeasurement;
+        private double scaleAmount;
+
+        // Constructor
+        public Ingredient(string name, double quantity, string unitOfMeasurement)
+        {
+            this.name = name;
+            this.quantity = quantity;
+            this.unitOfMeasurement = unitOfMeasurement;
+        }
+
+        // Property for ingredient name
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        // Property for ingredient quantity
+        public double Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+        // Property for ingredient unit of measurement
+        public string UnitOfMeasurement
+        {
+            get { return unitOfMeasurement; }
+            set { unitOfMeasurement = value; }
+        }
+
+        // Property for ingredient scale amount
+        public double ScaleAmount
+        {
+            get { return scaleAmount; }
+            set { scaleAmount = value; }
+        }
+
+    }
+}
+
---
+++ PROG6221-POE-ST10298850-LukePetzer/Program.cs
@@ -0,0 +1,137 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ST10298850_POE_LukePetzer.Classes;
+using System;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace ST10298850_POE
+{
+    // Main program class
+    class Program
+    {
+        // Declare a static instance of the Recipe class
+        private static Recipe Recipe = new Recipe();
+
+        // Main method
+        static void Main(string[] args)
+        {
+            // Welcome message
+            Console.WriteLine("Welcome to the Recipe App!");
+
+            // Retrieve the values entered by the user
+            Console.WriteLine("Please enter a name for your recipe.");
+            Recipe.Name = Console.ReadLine();
+            Recipe.SetIngredients(getIngredientFromUser());
+            Recipe.SetStepsDescriptions(getStepsDescriptionsFromUser());
+            DisplayFullRecipe();
+
+            bool loopContinue = true;
+            // Loop to continue displaying options until user chooses to exit
+            while (loopContinue)
+            {
+                // Prompt user for choice
+                Console.WriteLine("Please select what option you would like to enter.");
+                Console.WriteLine("\n 0. - To change the recipe scale. \n 1. - To reset the recipe scale back to the orignal. \n 2. Make a new Recipe.\n3. Display recipe \n 4. Exit program.");
+                int switchChoice = int.Parse(Console.ReadLine());
+
+                // Switch statement to handle user choice
+                switch (switchChoice)
+                {
+                    case 0:
+                        changeRecipeSize();
+                        DisplayFullRecipe();
+                        break;
+                    case 1:
+                        resettingOfRecipeScale();
+                        DisplayFullRecipe();
+                        break;
+                    case 2:
+                        Recipe.clearRecipe();
+                        Console.WriteLine("Please enter a name for your recipe.");
+                        Recipe.Name = Console.ReadLine();
+                        Recipe.SetIngredients(getIngredientFromUser());
+                        Recipe.SetStepsDescriptions(getStepsDescriptionsFromUser());
+                        break;
+                    case 3:
+                        DisplayFullRecipe();
+                        break;
+                    case 4:
+                        loopContinue = false;
+                        break;
+                    default:
+                        loopContinue = true;
+                        break;
+                }
+                if (loopContinue)
+                    Console.WriteLine("Please enter a valid choice.");
+            }
+        }
+
+        // Method to get ingredients from user input
+        private static Ingredient[] getIngredientFromUser()
+        {
+            Console.WriteLine("Please indicate how many ingredients this recipe contains.");
+            int noOfIngredients = int.Parse(Console.ReadLine());
+
+            Ingredient[] ingredients = new Ingredient[noOfIngredients];
+
+            for (int i = 0; i < noOfIngredients; i++)
+            {
+                Console.WriteLine("Please indicate the name of the ingredient.");
+                string ingredientName = Console.ReadLine();
+                Console.WriteLine("Please indicate the quantity of the ingredient.");
+                double ingredientQuantity = double.Parse(Console.ReadLine());
+                Console.WriteLine("Please indicate the unit of measurement of the ingredient.");
+                string ingredientUnitofMeasurement = Console.ReadLine();
+
+                ingredients[i] = new Ingredient(ingredientName, ingredientQuantity, ingredientUnitofMeasurement);
+            }
+
+            return ingredients;
+        }
+
+        // Method to get steps descriptions from user input
+        private static string[] getStepsDescriptionsFromUser()
+        {
+            Console.WriteLine("Please indicate the number of steps the recipe contains.");
+            int ingredientNoSteps = int.Parse(Console.ReadLine());
+
+            string[] stepsDescriptions = new string[ingredientNoSteps];
+
+            for (int j = 0; j < ingredientNoSteps; j++)
+            {
+                Console.WriteLine($"Please provide a description of step {j + 1}");
+                stepsDescriptions[j] = Console.ReadLine();
+            }
+
+            return stepsDescriptions;
+        }
+
+        // Method to display full recipe
+        private static void DisplayFullRecipe()
+        {
+            Console.WriteLine(Recipe.DisplayRecipe());
+        }
+
+        // Method to change recipe size
+        public static void changeRecipeSize()
+        {
+            Console.WriteLine("Please indicate to what scale you would like to change the recipe to?");
+            Console.WriteLine("The options are: half, double or triple.");
+
+            double scaleAmount = double.Parse(Console.ReadLine());
+            Recipe.UpdateRecipeScale(scaleAmount);
+
+            Console.WriteLine($"Recipe scaled by a factor of {scaleAmount}.");
+        }
+
+        // Method to reset recipe scale
+        public static void resettingOfRecipeScale()
+        {
+            Console.WriteLine("Please indicate if you would like to reset the recipe scale to 0.");
+
+            Recipe.ResettingOfRecipeScale();
+        }
+    }
+
+}
